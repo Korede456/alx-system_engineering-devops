fd98b82d90d9c8b3329e26ff78dc1e4885adf6ae
korede thompson <koredeodeyemi456@gmail.com>
2024-02-24 08:49:20 +0000

    C function that creates zombie processes

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -9,30 +9,31 @@
 
 int main(void)
 {
-    int i;
-    
-    // Creating 5 zombie processes
-    for (i = 0; i < 5; i++) {
-        pid_t pid = fork();
-        
-        if (pid < 0) {
-            perror(fork error);
-            exit(EXIT_FAILURE);
-        } else if (pid == 0) {
-            // Child process
-            exit(EXIT_SUCCESS);
-        } else {
-            // Parent process
-            printf(Zombie process created, PID: %dn, pid);
-            sleep(1); // Sleep for a short time to allow the parent to print the message
-        }
-    }
-    
-    // Function to keep the parent process running
-    while (1) {
-        sleep(1);
-    }
+	int i;
 
-    return 0;
+	for (i = 0; i < 5; i++)
+	{
+		pid_t pid = fork();
+
+		if (pid < 0)
+		{
+			perror(fork error);
+			exit(EXIT_FAILURE);
+		}
+		else if (pid == 0)
+		{
+			exit(EXIT_SUCCESS);
+		}
+		else
+		{
+			printf("Zombie process created, PID: %dn", pid);
+		}
+	}
+
+	while (1)
+	{
+		sleep(1);
+	}
+
+	return (0);
 }
-
