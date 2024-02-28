4b7d5bb812d203efd2de163fae4403530e286d37
korede thompson <koredeodeyemi456@gmail.com>
2024-02-28 14:00:47 +0000

    REGEXP

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,3 +1,17 @@
 #!/usr/bin/env ruby
+# Regex pattern to match sender, receiver, and flags
+pattern = /\[from:([^\]]+)\] \[to:([^\]]+)\] \[flags:([^\]]+)\]/
 
-puts ARGV[0].scan(/\[from:([^\]]+)\] \[to:([^\]]+)\] \[flags:([^\]]+)\]/).join
+input_string = ARGV[0]
+
+match_data = input_string.match(pattern)
+
+if match_data
+  sender = match_data[1]
+  receiver = match_data[2]
+  flags = match_data[3]
+
+  puts "#{sender},#{receiver},#{flags}"
+else
+  puts "No match found in the input string."
+end
